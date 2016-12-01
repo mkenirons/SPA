8b7bcac461b1bd641c56bd22107a89e397224d8e
teresakelly22 <teresakelly22@eircom.net>
2016-12-01 20:09:13 +0000

    Add files via upload

--- server.py
+++ server.py
@@ -1,30 +1,30 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-
+#for rendering the pages from the template folder
 @app.route('/')
 def Index():
     return render_template('index.html')
-	
+	#returns the index page from the template folder
 @app.route('/home')
 def Home():
 	return render_template('home.html')
-
+	#returns the home page from the template folder
 @app.route('/about')
 def About():
 	return render_template('about.html')
-
+	#returns the about page from the template folder
 @app.route('/prototype')
 def Prototype():
 	return render_template('prototype.html')
-	
+	#returns the prototype page from the template folder	
 @app.route('/contact')
 def Contact():
 	return render_template('contact.html')
-	
+	#returns the contact page from the template folder	
 @app.route('/map')
 def Map():
 	return render_template('map.html')
-	
+	#returns the map page from the template folder	
 if __name__ == "__main__":
     app.run()
 
